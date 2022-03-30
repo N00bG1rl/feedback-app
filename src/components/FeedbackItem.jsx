3eebf3e091a2dcbffb29d5c7bd92b96e5a14cf53
N00bG1rl <403helen@gmail.com>
2022-03-30 22:55:05 +0300

    Delete feedbacks

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,8 +1,9 @@
+import { FaTimes, FaEdit } from 'react-icons/fa'
 import PropTypes from 'prop-types'
 
 import Card from './UI/Card'
 
-function FeedbackItem({ item }) {
+function FeedbackItem({ item, handleDelete }) {
 	// const [rating, setRating] = useState(7)
 	// const [text, setText] = useState('Add text here...')
 
@@ -15,6 +16,9 @@ function FeedbackItem({ item }) {
 	return (
 		<Card>
 			<div className='num-display'>{item.rating}</div>
+			<button onClick={() => handleDelete(item.id)} className='close'>
+				<FaTimes color='red' />
+			</button>
 			<div className='text-display'>{item.text}</div>
 		</Card>
 	)
