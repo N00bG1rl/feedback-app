cccae6181a41526d895aea05c4a7fdfb279e7fc0
N00bG1rl <403helen@gmail.com>
2022-03-31 10:01:34 +0300

    Adding Feedback

--- src/components/feedback/FeedbackFrom.jsx
+++ src/components/feedback/FeedbackFrom.jsx
@@ -5,8 +5,21 @@ import Button from '../UI/Button'
 
 function FeedbackFrom() {
 	const [text, setText] = useState('')
+	const [btnDisabled, setBtnDisabled] = useState(true)
+	const [message, setMessage] = useState('')
 
 	const handleTextChange = event => {
+		if (!text.length) {
+			setBtnDisabled(true)
+			setMessage(null)
+		} else if (text !== '' && text.trim().length <= 10) {
+			setBtnDisabled(true)
+			setMessage('Text must be at least 10 characters')
+		} else {
+			setMessage(null)
+			setBtnDisabled(false)
+		}
+
 		setText(event.target.value)
 	}
 
@@ -19,10 +32,13 @@ function FeedbackFrom() {
 						onChange={handleTextChange}
 						value={text}
 						type='text'
-						placeholder='Write a rewiev'
+						placeholder='Write a review'
 					/>
-					<Button type='submit'>Send</Button>
+					<Button type='submit' isDisabled={btnDisabled}>
+						Send
+					</Button>
 				</div>
+				{message && <div className='message'>{message}</div>}
 			</form>
 		</Card>
 	)
