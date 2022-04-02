b9c4ac00fccffddab7c96a5bc68d83ab71d66979
N00bG1rl <403helen@gmail.com>
2022-04-02 10:14:25 +0300

    Added assets

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -1,21 +1,27 @@
-import { createContext, useState } from 'react'
+import { createContext, useState, useEffect } from 'react'
 import { v4 as uuidv4 } from 'uuid'
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({ children }) => {
-	const [feedback, setFeedback] = useState([
-		{
-			id: 1,
-			text: 'This is from context',
-			rating: 10,
-		},
-	])
-
+	const [isLoading, setIsLoading] = useState(true)
+	const [feedback, setFeedback] = useState([])
 	const [feedbackEdit, setFeedbackEdit] = useState({
 		item: {},
 		edit: false,
 	})
+
+	useEffect(() => {
+		fetchFeedback()
+	}, [])
+
+	const fetchFeedback = async () => {
+		const response = await fetch('http://localhost:5000/feedback')
+		const data = await response.json()
+
+		setFeedback(data)
+		setIsLoading(false)
+	}
 
 	const addFeedback = newFeedback => {
 		newFeedback.id = uuidv4()
@@ -48,6 +54,7 @@ export const FeedbackProvider = ({ children }) => {
 			value={{
 				feedback,
 				feedbackEdit,
+				isLoading,
 				deleteFeedback,
 				addFeedback,
 				editFeedback,
