a4b23b59cc1589c4453de798186b3de5bfce3d71
N00bG1rl <403helen@gmail.com>
2022-04-02 10:16:39 +0300

    Added loading state

--- src/components/feedback/FeedbackList.jsx
+++ src/components/feedback/FeedbackList.jsx
@@ -3,15 +3,18 @@ import { motion, AnimatePresence } from 'framer-motion'
 
 import FeedbackContext from '../../context/FeedbackContext'
 import FeedbackItem from './FeedbackItem'
+import Spinner from '../UI/Spinner'
 
 function FeedbackList() {
-	const { feedback } = useContext(FeedbackContext)
+	const { feedback, isLoading } = useContext(FeedbackContext)
 
-	if (!feedback || !feedback.length) {
+	if (!isLoading && (!feedback || !feedback.length)) {
 		return <p>No feedbacks yet. Add an feedback...</p>
 	}
 
-	return (
+	return isLoading ? (
+		<Spinner />
+	) : (
 		<div className='feedback-list'>
 			<AnimatePresence>
 				{feedback.map(item => (
